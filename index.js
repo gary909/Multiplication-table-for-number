3a2700d123d2ead5a95c24bd1f69e2b3b2f9e1e6
gary <gary.white@lodgit.com>
2022-02-03 13:51:45 +0100

    code

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function multiTable(number) {
     for (var i = 1; i <= 10; i++ ) {
-        console.log(`${i} * ${number} = ${i * number}`);
+        return `${i} * ${number} = ${i * number}`;
     }
+    // return number;
 }
 
 console.log(multiTable(5)); // return '1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50'
